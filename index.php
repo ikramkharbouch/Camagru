54be41e024194144b1f1743441816b6ea35382a4
ikramkharbouch <4573r14@gmail.com>
2020-01-26 22:01:03 +0100

    Add Landing page + signin form

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Camagru</title>
+    <link rel="icon" href="assets/ar-camera.ico">
+    <link rel="stylesheet" href="styles/main.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Raleway&display=swap" rel="stylesheet">
+
+</head>
+<body>
+    <header>
+        <img src="./assets/Camagru.png" alt="Camagru">
+        <ul>
+            <li><a href="#">Sign up</a></li>
+            <li><a href="./forms/signin.html">Sign in</a></li>
+            <li><a href="#">About</a></li>
+        </ul>
+    </header>
+    <section class="main"></section>
+    <footer>&copy; 2020</footer>
+</body>
+</html>
