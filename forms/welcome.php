3d2fc74fbb5811fdfe0bb8cbac958982f65ebb71
Ikram Kharbouch <ikrkharb@e1r5p15.1337.ma>
2020-10-16 20:22:17 +0100

    Update welcome.php page

--- forms/welcome.php
+++ forms/welcome.php
@@ -5,6 +5,9 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Welcome Page</title>
+  <link rel="stylesheet" href="../styles/welcome.css">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+  <script src="https://kit.fontawesome.com/a076d05399.js"></script>
 </head>
 
 <body>
@@ -14,18 +17,74 @@
     <button onclick="snap();">Snap</button>
     <a href="img.png" download="output.png">Download</a> -->
 
-  <div class="camera">
+    <div id="mySidenav" class="sidenav">
+      <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+      <h1>Camagru</h1>
+      <a href="#">Home</a>
+      <a href="#">Gallery</a>
+      <a href="#">Profile</a>
+      <a href="#">Settings</a>
+      <a href="#">Logout</a>
+    </div>
+
+  <!-- Use any element to open the sidenav -->
+  <span onclick="openNav()"><i class="fas fa-bars"></i>
+</span>
+
+<div class="user-details d-flex flex-row">
+<p>Ikram Kharbouch</p>
+<img src="../assets/avatar.png" alt="Avatar" class="avatar">
+</div>
+
+
+  <!-- Add all page content inside this div if you want the side nav to push page content to the right (not used if you only want the sidenav to sit on top of the page -->
+  <div id="main">
+    <div class="camera">
+    <button id="startbutton" type="button" class="btn btn-primary btn-lg">Start Capture</button>
+    <button id="startbutton" type="button" class="btn btn-danger btn-lg" onclick="clearphoto()">Reset</button>
     <video id="video">Video stream not available.</video>
-    <button id="startbutton">Take photo</button>
+    
   </div>
 
   <canvas id="canvas">
   </canvas>
-  <div class="output">
+  <!-- <div class="output">
     <img id="photo" alt="The screen capture will appear in this box.">
+  </div> -->
   </div>
 
+  <div class="emojis">
+    <h3>Add Emojis</h3>
+    <div class="container">
+  <div class="row row-cols-2">
+    <div class="col"><img src="../assets/emoji-inlove.png" alt=""></div>
+    <div class="col"><img src="../assets/emoji-inlove.png" alt=""></div>
+    <div class="col"><img src="../assets/emoji-inlove.png" alt=""></div>
+    <div class="col"><img src="../assets/emoji-inlove.png" alt=""></div>
+    <div class="col"><img src="../assets/emoji-inlove.png" alt=""></div>
+    <div class="col"><img src="../assets/emoji-inlove.png" alt=""></div>
+    <div class="col"><img src="../assets/emoji-inlove.png" alt=""></div>
+    <div class="col"><img src="../assets/emoji-inlove.png" alt=""></div>
+    <div class="col"><img src="../assets/emoji-inlove.png" alt=""></div>
+    <div class="col"><img src="../assets/emoji-inlove.png" alt=""></div>
+  </div>
+</div>
+  </div>
+
+
+
   <script type="text/javascript">
+  function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+
+}
     (function() {
       // The width and height of the captured photo. We will set the
       // width to the value defined here, but the height will be
@@ -137,7 +196,7 @@
 
 
 
-    
+
     // var video = document.getElementById('video');
     // var canvas = document.getElementById('canvas');
     // var context = canvas.getContext('2d');
@@ -172,7 +231,9 @@
     // //     var newData = dataURL.replace("image/png", "image/octet-stream");
     // //     download.attr("download", "VaishIvfBiodata.png").attr("href", newData);
     // // }
-  </script>
+
+    /* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+</script>
 </body>
 
 </html>
--- index.php
+++ index.php
@@ -20,6 +20,7 @@
       integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
       crossorigin="anonymous"
     />
+    <link href="https://fonts.googleapis.com/css2?family=Jacques+Francois&family=Roboto:wght@100;300;400;500;700&display=swap" rel="stylesheet">
   </head>
 
   <body>
