d73112071ca77746058b83160be84cf1bdfb7c67
Ikram Kharbouch <ikrkharb@e2r2p5.1337.ma>
2020-10-26 17:44:06 +0100

    Change Host

--- config/Database.php
+++ config/Database.php
@@ -1,7 +1,7 @@
 <?php
     class Database {
         // DB Params
-        private $host = '54.163.108.123';
+        private $host = 'localhost';
         private $db_name = 'camagru';
         private $username = 'root';
         private $password = 'Root1234@';
@@ -11,7 +11,7 @@
         public function connect() {
             $this->conn = null; 
             try {
-                $this->conn = new PDO('mysql:host=54.163.108.123' . ';port=3306' . ';dbname=' . $this->db_name,
+                $this->conn = new PDO('mysql:host=localhost' . ';port=3306' . ';dbname=' . $this->db_name,
                 // $this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->db_name,
                 $this->username, $this->password);
                 $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
