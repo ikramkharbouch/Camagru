66f4c457af66aa83ce456aec4b2146b8039c09c5
Ikram Kharbouch <ikrkharb@e2r2p5.1337.ma>
2020-10-26 17:28:59 +0100

    Change Path

--- api/post/read.php
+++ api/post/read.php
@@ -3,8 +3,8 @@
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
 
-include_once '../../Camagru/config/Database.php';
-include_once '../../Camagru/models/User.php';
+include_once '../../config/Database.php';
+include_once '../../models/User.php';
 
 // Instantiate DB & connect
 $database = new Database();
