53a404ea7f94fe5ccc7ee9af408e944d8841c824
Ikram Kharbouch <ikrkharb@e1r9p11.1337.ma>
2020-11-01 14:01:50 +0100

    debug

--- models/User.php
+++ models/User.php
@@ -120,10 +120,6 @@
     // Create new user
     public function create() {
         // Create Query
-
-       $lastid = LAST_INSERT_ID();
-
-       var_dump($lastid);
        $query = 'BEGIN;
        INSERT INTO users SET email = :email, username = :username, pass = :pass, verified = :verified, token = :token;
        INSERT INTO posts SET user_id = 27;
