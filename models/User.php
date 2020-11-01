ebde2e8aa9836ba60158efe2bf69e2b52e7c1d9d
Ikram Kharbouch <ikrkharb@e1r9p11.1337.ma>
2020-11-01 13:47:03 +0100

    debug

--- models/User.php
+++ models/User.php
@@ -120,7 +120,10 @@
     // Create new user
     public function create() {
         // Create Query
-       $query = 'INSERT INTO users SET email = :email, username = :username, pass = :pass, verified = :verified, token = :token';
+       $query = 'BEGIN;
+       INSERT INTO users SET email = :email, username = :username, pass = :pass, verified = :verified, token = :token;
+       INSERT INTO posts SET user_id = 1;
+       COMMIT;';
 
     //    $query = "INSERT INTO users (email, username, pass, verified, token) VALUES ('test@test.fr', 'tester21', 'test@hh1423', '0', 'hdhufgeiuf')";
 
