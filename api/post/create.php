472ae2da4b3f12734e14abbdfa59373dd6c3d00e
Ikram Kharbouch <ikrkharb@e1r8p11.1337.ma>
2020-11-04 13:32:34 +0100

    Add phpmailer

--- api/post/create.php
+++ api/post/create.php
@@ -5,9 +5,7 @@ header('Content-Type: application/json');
 header('Access-Control-Allow-Methods: POST');
 header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With');
 
-// Import the Postmark Client Class:
-require_once('../../vendor/autoload.php');
-use Postmark\PostmarkClient;
+require_once('../../PHPMailer/PHPMailerAutoload.php');
 
 include_once '../../config/Database.php';
 include_once '../../models/User.php';
@@ -36,32 +34,40 @@ if ($user->check()) {
 } else {
     if ($user->create()) {
 
-        // $client = new PostmarkClient("POSTMARK-SERVER-API-TOKEN-HERE");
+        $mail = new PHPMailer();
+        $mail->isSMTP();
+        $mail->SMTPAuth = true;
+        $mail->SMTPSecure = 'ssl';
+        $mail->Host = 'smtp.gmail.com';
+        $mail->Port = '465';
+        $mail->isHTML();
+        $mail->Username = '4573r14@gmail.com';
+        $mail->Password = 'Fildefer1234@';
 
-        // // Send an email:
-        // $sendResult = $client->sendEmail(
-        //     "Camagru",
-        //     "4573r14@gmail.com",
-        //     "Hello from Postmark!",
-        //     "This is just a friendly 'hello' from your friends at Postmark."
-        // );
-        $to = $user->email;
-        $subject = "Email Verification";
+        $mail->SetFrom('no-reply@camagru.ml');
+        $mail->Subject = 'Hello World';
+        $mail->Body = 'A test mail';
+        $mail->AddAddress($user->email);
 
-        $message = "<body>
-                    <h1>Confirm Your Email</h1>
-                    Please confirm your email address " . "<a href='http://http://54.163.108.123/Camagru/api/post/verify.php?token=$user->token'>Verify Account</a>
-                    </body>";
+        $mail->Send();
 
-        // Always set content-type when sending HTML email
-        $headers = "MIME-Version: 1.0" . "\r\n";
-        $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+        // $to = $user->email;
+        // $subject = "Email Verification";
 
-        // More headers
-        $headers .= 'From: <webmaster@example.com>' . "\r\n";
-        $headers .= 'Cc: myboss@example.com' . "\r\n";
+        // $message = "<body>
+        //             <h1>Confirm Your Email</h1>
+        //             Please confirm your email address " . "<a href='http://54.163.108.123/Camagru/api/post/verify.php?token=$user->token'>Verify Account</a>
+        //             </body>";
 
-        mail($to, $subject, $message, $headers);
+        // // Always set content-type when sending HTML email
+        // $headers = "MIME-Version: 1.0" . "\r\n";
+        // $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+
+        // // More headers
+        // $headers .= 'From: <webmaster@example.com>' . "\r\n";
+        // $headers .= 'Cc: myboss@example.com' . "\r\n";
+
+        // mail($to, $subject, $message, $headers);
 
         echo json_encode(
             array('Message' => 'Post Created')
