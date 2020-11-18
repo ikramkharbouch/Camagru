c37720e74a0c1e2a8cca93ad92b86bd812cd3edd
Ikram Kharbouch <ikrkharb@e1r5p10.1337.ma>
2020-11-18 20:20:05 +0100

    debug

--- api/post/create.php
+++ api/post/create.php
@@ -31,9 +31,15 @@ if ($user->check()) {
     );
 } else {
     if ($user->create()) {
-        
 
-        var_dump(mail("geekgirl6667@gmail.com", "Confirming Camagru account", "php on"));
+        $to      = $user->email;
+        $subject = 'Confirming Camagru Account';
+        $message = "<body>
+                    <h1>Confirm Your Email</h1>
+                    Please confirm your email address " . "<a href='http://34.90.29.10/api/post/verify.php?token=$user->token'>Verify Account</a>
+                    </body>";
+
+        mail($to, $subject, $message);
 
         // $to      = $user->email;
         // $subject = 'the subject';
