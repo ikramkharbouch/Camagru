8da4121c25e5a5e9896cd5406f12839f96d90678
Ikram Kharbouch <ikrkharb@e1r7p15.1337.ma>
2020-11-20 13:33:08 +0100

    debug

--- forms/signup.php
+++ forms/signup.php
@@ -68,7 +68,7 @@
         let email = document.getElementById("email").value;
         let pass = document.getElementById("pass").value;
 
-        fetch("http://34.90.29.10/api/post/create.php", {
+        fetch("https://34.90.29.10/api/post/create.php", {
           method: "POST",
           headers: {
             'Content-Type': 'application/json',
@@ -93,7 +93,7 @@
       }
 
       function getUsers() {
-        fetch("http://34.90.29.10/api/post/read_single.php?id=2")
+        fetch("https://34.90.29.10/api/post/read_single.php?id=2")
           .then((res) => res.json())
           .then((data) => console.log(data))
           .catch((err) => console.log(err));
