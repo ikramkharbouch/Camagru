56cdc3565362afeb419db68e5d66b5ca53fae1a2
Ikram Kharbouch <ikrkharb@e1r7p15.1337.ma>
2020-11-20 13:54:21 +0100

    change email styling

--- api/post/create.php
+++ api/post/create.php
@@ -34,9 +34,16 @@ if ($user->check()) {
 
         $to      = $user->email;
         $subject = 'Confirming Camagru Account';
-        $message = "<body>
+        $message = "<body style='text-align:center;'>
                     <h1>Confirm Your Email</h1>
-                    Please confirm your email address " . "<a href='http://34.90.29.10/api/post/verify.php?token=$user->token'>Verify Account</a>
+                    Please confirm your email address " . "<a href='http://34.90.29.10/api/post/verify.php?token=$user->token'><button style='background-color: #4CAF50; /* Green */
+                    border: none;
+                    color: white;
+                    padding: 15px 32px;
+                    text-align: center;
+                    text-decoration: none;
+                    display: inline-block;
+                    font-size: 16px;'>Verify</button></a>
                     </body>";
 
         mail($to, $subject, $message);
