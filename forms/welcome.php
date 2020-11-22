11be3aaaeeef04cc475aad47b1e1a4b1ccd530e1
ikramkharbouch <4573r14@gmail.com>
2020-11-22 18:24:10 +0000

    Update welcome.php

--- forms/welcome.php
+++ forms/welcome.php
@@ -1,4 +1,4 @@
-<!-- <!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
 
 <head>
@@ -13,91 +13,58 @@
 
 <body>
 
-    <div id="mySidenav" class="sidenav">
-      <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-      <h1>Camagru</h1>
-      <a href="../index.php">Home</a>
-      <a href="#">Gallery</a>
-      <a href="#">Profile</a>
-      <a href="#">Settings</a>
-      <a href="#">Logout</a>
-    </div> -->
+  <div id="mySidenav" class="sidenav">
+    <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+    <h1>Camagru</h1>
+    <a href="../index.php">Home</a>
+    <a href="#">Gallery</a>
+    <a href="#">Profile</a>
+    <a href="#">Settings</a>
+    <a href="#">Logout</a>
+  </div> -->
 
   <!-- Use any element to open the sidenav -->
-  <!-- <span onclick="openNav()"><i class="fas fa-bars"></i>
-</span>
+  <span onclick="openNav()"><i class="fas fa-bars"></i>
+  </span>
 
-<div class="user-details d-flex flex-row">
-<p>Ikram Kharbouch</p>
-<img src="../assets/avatar.png" alt="Avatar" class="avatar">
-</div> -->
+  <div class="user-details d-flex flex-row">
+    <p>Ikram Kharbouch</p>
+    <img src="../assets/avatar.png" alt="Avatar" class="avatar">
+  </div>
 
 
   <!-- Add all page content inside this div if you want the side nav to push page content to the right (not used if you only want the sidenav to sit on top of the page -->
-  <!-- <div id="main">
-  <div class="camera">
-    <video id="video">Video stream not available.</video>
-    <button id="startbutton" type="button" class="btn btn-success">Take photo</button> 
-  </div>
-  <canvas id="canvas">
-  </canvas>
-  <div class="output">
-    <img id="photo" alt="The screen capture will appear in this box."> 
-  </div>
-  <div id="my-image"></div>
+  <div id="main">
+    <div class="camera">
+      <video id="video">Video stream not available.</video>
+      <button id="startbutton" type="button" class="btn btn-success">Take photo</button>
+    </div>
+    <canvas id="canvas">
+    </canvas>
+    <div class="output">
+      <img id="photo" alt="The screen capture will appear in this box.">
+    </div>
+    <div id="my-image"></div>
 
-  <div class="emojis">
-    <h3>Add Emojis</h3>
-    <div class="container">
-  <div class="row row-cols-2">
-    <div class="col"><img src="../assets/emoji-inlove.png" alt=""></div>
-    <div class="col"><img src="../assets/emoji-inlove.png" alt=""></div>
-    <div class="col"><img src="../assets/emoji-inlove.png" alt=""></div>
-    <div class="col"><img src="../assets/emoji-inlove.png" alt=""></div>
-    <div class="col"><img src="../assets/emoji-inlove.png" alt=""></div>
-    <div class="col"><img src="../assets/emoji-inlove.png" alt=""></div>
-    <div class="col"><img src="../assets/emoji-inlove.png" alt=""></div>
-    <div class="col"><img src="../assets/emoji-inlove.png" alt=""></div>
-    <div class="col"><img src="../assets/emoji-inlove.png" alt=""></div>
-    <div class="col"><img src="../assets/emoji-inlove.png" alt=""></div>
-  </div>
-</div>
-  </div>
+    <div class="emojis">
+      <h3>Add Emojis</h3>
+      <div class="container">
+        <div class="row row-cols-2">
+          <div class="col"><img src="../assets/emoji-inlove.png" alt=""></div>
+          <div class="col"><img src="../assets/emoji-inlove.png" alt=""></div>
+          <div class="col"><img src="../assets/emoji-inlove.png" alt=""></div>
+          <div class="col"><img src="../assets/emoji-inlove.png" alt=""></div>
+          <div class="col"><img src="../assets/emoji-inlove.png" alt=""></div>
+          <div class="col"><img src="../assets/emoji-inlove.png" alt=""></div>
+          <div class="col"><img src="../assets/emoji-inlove.png" alt=""></div>
+          <div class="col"><img src="../assets/emoji-inlove.png" alt=""></div>
+          <div class="col"><img src="../assets/emoji-inlove.png" alt=""></div>
+          <div class="col"><img src="../assets/emoji-inlove.png" alt=""></div>
+        </div>
+      </div>
+    </div>
   </div>
   </div>
 </body>
 
-</html> -->
-
-
-<!doctype html>
-<html>
-<head>
-	<title>WebRTC: Still photo capture demo</title>
-	<meta charset='utf-8'>
-	<script src="camera.js">
-	</script>
-</head>
-<body>
-<div class="contentarea">
-	<h1>
-		MDN - WebRTC: Still photo capture demo
-	</h1>
-	<p>
-		This example demonstrates how to set up a media stream using your built-in webcam, fetch an image from that stream, and create a PNG using that image.
-	</p>
-  <div class="camera">
-    <video id="video">Video stream not available.</video>
-    <button id="startbutton">Take photo</button> 
-  </div>
-  <canvas id="canvas">
-  </canvas>
-  <div class="output">
-    <img id="photo" alt="The screen capture will appear in this box."> 
-  </div>
-	<p>
-		Visit our article <a href="https://developer.mozilla.org/en-US/docs/Web/API/WebRTC_API/Taking_still_photos"> Taking still photos with WebRTC</a> to learn more about the technologies used here.
-	</p>
-</div>
-</body>
 </html>
