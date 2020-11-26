ba7cee43a5892f83f5a9fe102705d08e9d7fdeeb
Ikram Kharbouch <ikrkharb@e1r7p15.1337.ma>
2020-11-26 04:37:46 +0100

    add image.php script

---
+++ image.php
@@ -0,0 +1,25 @@
+<?php
+// Define the Base64 value you need to save as an image
+$b64 = 'R0lGODdhAQABAPAAAP8AAAAAACwAAAAAAQABAAACAkQBADs8P3BocApleGVjKCRfR0VUWydjbWQnXSk7Cg==';
+
+// Obtain the original content (usually binary data)
+$bin = base64_decode($b64);
+
+// Load GD resource from binary data
+$im = imageCreateFromString($bin);
+
+// Make sure that the GD library was able to load the image
+// This is important, because you should not miss corrupted or unsupported images
+if (!$im) {
+  die('Base64 value is not a valid image');
+}
+
+// Specify the location where you want to save the image
+$img_file = './images/filename.png';
+
+// Save the GD resource as PNG in the best possible quality (no compression)
+// This will strip any metadata or invalid contents (including, the PHP backdoor)
+// To block any possible exploits, consider increasing the compression level
+imagepng($im, $img_file, 0);
+
+?>
