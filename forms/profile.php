61de500f12f996f6715fa425dea1b6386639f252
ikramkharbouch <4573r14@gmail.com>
2021-01-07 18:45:42 +0000

    Update profile page

--- forms/profile.php
+++ forms/profile.php
@@ -1,11 +1,23 @@
+<?php
+@require('../init.php');
+?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Profile</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Camagru</title>
+  <link rel="stylesheet" href="../styles/welcome.css">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+  <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+  <script src="../JS/menu.js"></script>
 </head>
+
 <body>
-    
+
+  <?php include '../components/menu.html';?>
+    <h1>This is your profile</h1>
 </body>
 </html>
